c62148fa28011efe5587dcb17e8f9606e3c7d4f8
himel2535 <monwarhossanhimel@gmail.com>
2025-09-05 02:18:49 +0600

    making card dynamic

--- script/index.js
+++ script/index.js
@@ -13,15 +13,30 @@ const loadLevelWord=(id)=>{
     .then(data=>displayLevelWord(data.data))
 }
 
+
+// id: 162
+// level: 6
+// meaning: "অত্যাচারী"
+// pronunciation: "নেফ্যারিয়াস"
+// word: "Nefarious"
+
 const displayLevelWord=(words)=>{
     const wordContainer=document.getElementById("word-container");
-    // wordContainer.innerHTML=""
+    wordContainer.innerHTML=""
 
     for(let word of words){
         console.log(word)
         const card=document.createElement("div")
         card.innerHTML=`
-        <p>cat</p>
+              <div class="bg-white rounded-xl shadow-sm text-center py-10 px-7 space-y-4">
+        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="text-2xl font-medium font-bangla">${word.meaning} /${word.pronunciation}</div>
+        <div class="flex justify-between items-center ">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
         `;
         wordContainer.append(card)
     }
