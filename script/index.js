cf494d1f7c77e0779ddde31ea9366bce16ce596d
himel2535 <monwarhossanhimel@gmail.com>
2025-09-05 01:48:57 +0600

    making card for dynamically set

--- script/index.js
+++ script/index.js
@@ -6,6 +6,27 @@ const loadLessons = () => {
 };
 loadLessons();
 
+const loadLevelWord=(id)=>{
+    const url=`https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayLevelWord(data.data))
+}
+
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById("word-container");
+    // wordContainer.innerHTML=""
+
+    for(let word of words){
+        console.log(word)
+        const card=document.createElement("div")
+        card.innerHTML=`
+        <p>cat</p>
+        `;
+        wordContainer.append(card)
+    }
+}
+
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
@@ -13,7 +34,7 @@ const displayLessons = (lessons) => {
   for (let lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-            <button href="" class="btn btn-outline btn-primary">
+            <button onclick="loadLevelWord(${lesson.level_no})" href="" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open">
             </i>Lesson - ${lesson.level_no}</button>
         `;
