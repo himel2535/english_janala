e92b856852b78781cda3e4ed1095c6de2a918c47
himel2535 <monwarhossanhimel@gmail.com>
2025-09-05 17:50:43 +0600

    Handle Errors , empty lessons for Better UX

--- script/index.js
+++ script/index.js
@@ -6,13 +6,12 @@ const loadLessons = () => {
 };
 loadLessons();
 
-const loadLevelWord=(id)=>{
-    const url=`https://openapi.programming-hero.com/api/level/${id}`
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayLevelWord(data.data))
-}
-
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
 
 // id: 162
 // level: 6
@@ -20,27 +19,38 @@ const loadLevelWord=(id)=>{
 // pronunciation: "নেফ্যারিয়াস"
 // word: "Nefarious"
 
-const displayLevelWord=(words)=>{
-    const wordContainer=document.getElementById("word-container");
-    wordContainer.innerHTML=""
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
 
-    for(let word of words){
-        console.log(word)
-        const card=document.createElement("div")
-        card.innerHTML=`
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+          <div class="text-center rounded-xl col-span-full space-y-6 font-bangla">
+      <img class="mx-auto" src="./assets/alert-error.png" alt="">
+      <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>
+      `;
+    return;
+  }
+
+  for (let word of words) {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
               <div class="bg-white rounded-xl shadow-sm text-center py-10 px-7 space-y-4">
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <div class="text-2xl font-medium font-bangla">${word.meaning} /${word.pronunciation}</div>
+        <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning :"অর্থ পাওয়া যায়নি"} /${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}</div>
         <div class="flex justify-between items-center ">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
         `;
-        wordContainer.append(card)
-    }
-}
+    wordContainer.append(card);
+  }
+};
 
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container");
@@ -53,6 +63,6 @@ const displayLessons = (lessons) => {
             <i class="fa-solid fa-book-open">
             </i>Lesson - ${lesson.level_no}</button>
         `;
-    levelContainer.appendChild(btnDiv)
+    levelContainer.appendChild(btnDiv);
   }
 };
